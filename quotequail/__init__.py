3932d20708ddbc81133ea5f825d30291039433a6
Thomas Steinacher <tom@eggdrop.ch>
2013-04-27 19:46:11 -0700

    Ability to unwrap forwarded emails / replies

--- quotequail/__init__.py
+++ quotequail/__init__.py
@@ -1,9 +1,61 @@
+# -*- coding: utf-8 -*-
 # quotequail
 # a library that identifies quoted text in email messages
 
 import re
 
 __all__ = ['quote']
+
+REPLY_PATTERNS = [
+    '^On .*wrote:$', # apple mail/gmail reply
+    '^Am .*schrieb .*:$', # German
+    '[0-9]{4}/[0-9]{1,2}/[0-9]{1,2} .* <.*@.*>$', # gmail (?) reply
+]
+
+FORWARD_MESSAGES = [
+    # apple mail forward
+    'Begin forwarded message', 'Anfang der weitergeleiteten E-Mail',
+
+    # gmail/evolution forward
+    'Forwarded [mM]essage',
+
+    # outlook
+    'Original [mM]essage', 'Ursprüngliche Nachricht', 'Mensaje [oO]riginal',
+]
+
+FORWARD_PATTERNS = [
+    '^________________________________$', # yahoo?
+] + ['^---+ ?%s ?---+$' % p for p in FORWARD_MESSAGES] \
+  + ['^%s:$' % p for p in FORWARD_MESSAGES]
+
+HEADER_MAP = {
+    'from': 'from',
+    'von': 'from',
+    'de': 'from',
+
+    'to': 'to',
+    'an': 'to',
+    'para': 'to',
+
+    'cc': 'cc',
+    'kopie': 'cc',
+
+    'bcc': 'bcc',
+    'blindkopie': 'bcc',
+
+    'reply-to': 'reply-to',
+
+    'date': 'date',
+    'sent': 'date',
+    'datum': 'date',
+    'enviado el': 'date',
+    'enviados': 'date',
+    'fecha': 'date',
+
+    'subject': 'subject',
+    'betreff': 'subject',
+    'asunto': 'subject',
+}
 
 """
 Takes a plain text message as an argument, returns a list of tuples.
@@ -13,19 +65,13 @@ E.g. [(True, 'expanded text'), (False, 'Some quoted text')]
 def quote(text):
     lines = text.split('\n')
 
-    regexes = [
-        '^On .*wrote:$', # apple mail/gmail reply
-        '^Am .*schrieb .*:$', # German
-        '[0-9]{4}/[0-9]{1,2}/[0-9]{1,2} .* <.*@.*>$', # gmail (?) reply
-        '^________________________________$', # yahoo?
-        '^Begin forwarded message:$', # apple mail forward
-        '^---+ ?Forwarded message ?---+$', # gmail forward
-        '^---+ ?Original Message ?---+$', # outlook?
-    ]
+    regexes = REPLY_PATTERNS + FORWARD_PATTERNS
     
     found = None
 
     for n, line in enumerate(lines):
+        if found != None:
+            break
         for regex in regexes:
             if re.match(regex, line.strip()):
                 found = n
@@ -35,3 +81,175 @@ def quote(text):
         return [(True, '\n'.join(lines[:found+1])), (False, '\n'.join(lines[found+1:]))]
 
     return [(True, text)]
+
+
+"""
+If the passed text is the text body of a forwarded message, a dictionary with the following keys is returned:
+- type: "reply", "forward" or "quote"
+- text_top: Text at the top of the passed message (if found)
+- text_bottom: Text at the bottom of the passed message (if found)
+- from / to / subject / cc / bcc / reply-to: Corresponding header of the forwarded message, if it exists. (if found)
+- text: Text of the forwarded message (if found)
+
+Otherwise, this function returns None.
+
+"""
+def unwrap(text):
+    result = {}
+
+    lines = text.split('\n')
+
+    header_re = re.compile(r'\*?([a-zA-Z- ]+):\*?(.*)$')
+
+    pattern_map = {
+        'reply': REPLY_PATTERNS,
+        'forward': FORWARD_PATTERNS,
+    }
+
+    def _find_start(lines):
+        """
+        Find starting point of wrapped email. Returns a tuple containing
+        (line_number, type) where type can be one of the following:
+         * 'forward': A matching forwarding pattern was found
+         * 'reply': A matching repy pattern was found
+         * 'headers': Headers were found (usually a forwarded email)
+         * 'quote': A quote was found
+        Returns (None, None) if nothing was found.
+        """
+
+        # minimum number of lines to tell that this is actually
+        _min_headers = 3
+        _min_quoted = 3
+
+        for n, line in enumerate(lines):
+            if not line.strip():
+                continue
+
+            # Find a forward / reply start pattern
+            for typ, regexes in pattern_map.iteritems():
+                for regex in regexes:
+                    if re.match(regex, line.strip()):
+                        return n, typ
+
+            # Find a quote
+            if line.startswith('>'):
+                # Check if there are at least _min_quoted lines  that match
+                matched_lines = 1
+                for line in lines[n+1:]:
+                    if not line.strip():
+                        continue
+                    if not line.startswith('>'):
+                        break
+                    else:
+                        matched_lines += 1
+                    if matched_lines == _min_quoted:
+                        return n, 'quoted'
+
+            # Find a header
+            match = header_re.match(line)
+            if match:
+                # Check if there are at least _min_headers lines that match
+                matched_lines = 1
+                for line in lines[n+1:]:
+                    if not line.strip():
+                        continue
+                    if not header_re.match(line):
+                        break
+                    else:
+                        matched_lines += 1
+                    if matched_lines == _min_headers:
+                        return n, 'headers'
+
+        return None, None
+
+    def _unindent(lines):
+        unquoted = []
+        for n, line in enumerate(lines):
+            if line.startswith('> '):
+                unquoted.append(line[2:])
+            elif line.startswith('>'):
+                unquoted.append(line[1:])
+            else:
+                break
+
+        return unquoted, lines[n:]
+
+    def _extract_headers(lines):
+        hdrs = {}
+        for n, line in enumerate(lines):
+            if not line.strip():
+                continue
+
+            match = header_re.match(line)
+            if match:
+                header_name, header_value = match.groups()
+                header_name = header_name.strip().lower()
+
+                if header_name in HEADER_MAP:
+                    hdrs[HEADER_MAP[header_name]] = header_value.strip()
+            else:
+                # no more headers found
+                break
+
+        return hdrs, lines[n:]
+
+    def _unwrap(lines):
+        start, typ = _find_start(lines)
+        if typ in ('forward', 'reply'):
+            main_type = typ
+            start2, typ = _find_start(lines[start+1:])
+
+            if typ == 'quoted':
+                unquoted, rest = _unindent(lines[start+1+start2:])
+                start3, typ = _find_start(unquoted)
+                if typ == 'headers':
+                    hdrs, rest2 = _extract_headers(unquoted[start3:])
+                    return main_type, lines[:start], hdrs, rest2, rest
+                else:
+                    return main_type, lines[:start], None, unquoted, rest
+            elif typ == 'headers':
+                hdrs, rest = _extract_headers(lines[start+1:])
+                return main_type, lines[:start], hdrs, rest, []
+            else:
+                return main_type, lines[:start], None, lines[start+start2+1:], []
+
+        elif typ == 'headers':
+            main_type = 'forward'
+            hdrs, rest = _extract_headers(lines[start:])
+            return main_type, lines[:start], hdrs, rest, ''
+
+        elif typ == 'quoted':
+            unquoted, rest = _unindent(lines[start:])
+            start2, typ = _find_start(unquoted)
+            if typ == 'headers':
+                main_type = 'forward'
+                hdrs, rest2 = _extract_headers(unquoted[start2:])
+                return main_type, lines[:start], hdrs, rest2, rest
+            else:
+                main_type = 'quote'
+                return main_type, lines[:start], None, unquoted, rest
+
+
+    result = _unwrap(lines)
+    if result:
+        typ, text_top, hdrs, text, text_bottom = result
+
+        result = {
+            'type': typ,
+        }
+
+        text = '\n'.join(text).strip()
+        text_top = '\n'.join(text_top).strip()
+        text_bottom = '\n'.join(text_bottom).strip()
+
+        if text:
+            result['text'] = text
+        if text_top:
+            result['text_top'] = text_top
+        if text_bottom:
+            result['text_bottom'] = text_bottom
+
+        if hdrs:
+            result.update(hdrs)
+
+        return result
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 import unittest
-from quotequail import quote
+from quotequail import quote, unwrap
 
 class QuoteTestCase(unittest.TestCase):
     def test_quote(self):
@@ -43,6 +45,211 @@ Some quoted text.
             [(True, 'Hello world.\n\n---------- Forwarded message ----------'),
              (False, 'From: Someone <someone@example.com>\nSubject: The email\n\nSome quoted text.\n')]
         )
+
+
+class UnwrapTestCase(unittest.TestCase):
+    def test_unwrap(self):
+        # TODO: Test this function with replies
+
+        # Gmail forward
+        self.assertEqual(unwrap("""Hello
+
+---------- Forwarded message ----------
+From: Someone <noreply@example.com>
+Date: Fri, Apr 26, 2013 at 8:13 PM
+Subject: Weekend Spanish classes
+To: recipient@example.com
+
+Spanish Classes
+Learn Spanish
+"""), {
+            'text_top': 'Hello',
+            'type': 'forward',
+            'from': 'Someone <noreply@example.com>',
+            'date': 'Fri, Apr 26, 2013 at 8:13 PM',
+            'subject': 'Weekend Spanish classes',
+            'to': 'recipient@example.com',
+            'text': 'Spanish Classes\nLearn Spanish',
+        })
+
+        # Apple Mail forward
+        self.assertEqual(unwrap("""Hello
+
+Begin forwarded message:
+
+> From: "Some One" <some.one@example.com>
+> Date: 1. August 2011 23:28:15 GMT-07:00
+> To: "Other Person" <other@example.com>
+> Subject: AW: AW: Some subject
+> 
+> Original text
+
+Text bottom
+"""), {
+            'text_top': 'Hello',
+            'type': 'forward',
+            'from': '"Some One" <some.one@example.com>',
+            'date': '1. August 2011 23:28:15 GMT-07:00',
+            'subject': 'AW: AW: Some subject',
+            'to': '"Other Person" <other@example.com>',
+            'text': 'Original text',
+            'text_bottom': 'Text bottom',
+        })
+
+        # Sparrow forward
+        self.assertEqual(unwrap("""Hello
+
+Forwarded message:
+
+> From: Some One <some.one@example.com>
+> To: Other person <other@example.com>
+> Date: Thursday, March 7, 2013 7:09:41 PM
+> Subject: Re: Syncing Up
+>
+> OHAI
+>
+> Great news!
+
+Text bottom
+"""), {
+            'text_top': 'Hello',
+            'type': 'forward',
+            'from': 'Some One <some.one@example.com>',
+            'date': 'Thursday, March 7, 2013 7:09:41 PM',
+            'subject': 'Re: Syncing Up',
+            'to': 'Other person <other@example.com>',
+            'text': 'OHAI\n\nGreat news!',
+            'text_bottom': 'Text bottom',
+        })
+
+        # Forwrad with *bold* text
+        self.assertEqual(unwrap("""Hello
+
+Forwarded message:
+
+*From:* Some One <some@example.com>
+*To:* Other Person <other@example.com>
+*Date:* Wednesday, February 6, 2013 7:46:53 AM
+*Subject:* Fwd: Hottest Startups
+
+This is interesting."""), {
+            'text_top': 'Hello',
+            'type': 'forward',
+            'from': 'Some One <some@example.com>',
+            'date': 'Wednesday, February 6, 2013 7:46:53 AM',
+            'subject': 'Fwd: Hottest Startups',
+            'to': 'Other Person <other@example.com>',
+            'text': 'This is interesting.',
+        })
+
+        # No forwarding message text
+        self.assertEqual(unwrap("""Hello
+
+From: "Some One" <some.one@example.com>
+Date: 1. August 2011 23:28:15 GMT-07:00
+To: "Other Person" <other@example.com>
+Subject: AW: AW: Some subject
+
+Original text
+"""), {
+            'text_top': 'Hello',
+            'type': 'forward',
+            'from': '"Some One" <some.one@example.com>',
+            'date': '1. August 2011 23:28:15 GMT-07:00',
+            'subject': 'AW: AW: Some subject',
+            'to': '"Other Person" <other@example.com>',
+            'text': 'Original text',
+        })
+
+        # No forwarding message text
+        self.assertEqual(unwrap("""Hello
+
+> From: "Some One" <some.one@example.com>
+> Date: 1. August 2011 23:28:15 GMT-07:00
+> To: "Other Person" <other@example.com>
+> Subject: AW: AW: Some subject
+>
+> Original text
+"""), {
+            'text_top': 'Hello',
+            'type': 'forward',
+            'from': '"Some One" <some.one@example.com>',
+            'date': '1. August 2011 23:28:15 GMT-07:00',
+            'subject': 'AW: AW: Some subject',
+            'to': '"Other Person" <other@example.com>',
+            'text': 'Original text',
+        })
+
+        # Outlook?
+        self.assertEqual(unwrap("""-------- Original Message --------
+Subject: \tSome Newsletter
+Date: \tFri, 19 Jun 2009 19:16:04 +0200
+From: \tfrom <from@example.com>
+Reply-To: \treply <reply@example.com>
+To: \tto@example.com
+
+OHAI"""), {
+            'type': 'forward',
+            'from': 'from <from@example.com>',
+            'reply-to': 'reply <reply@example.com>',
+            'date': 'Fri, 19 Jun 2009 19:16:04 +0200',
+            'subject': 'Some Newsletter',
+            'to': 'to@example.com',
+            'reply-to': 'reply <reply@example.com>',
+            'text': 'OHAI',
+        })
+
+
+        # Some clients (Blackberry?) have weird whitespace rules
+        self.assertEqual(unwrap("""hello world
+
+-----Original Message-----
+From: "Some One" <some.one@example.com>
+
+Date: Sat, 22 Mar 2008 12:16:06 
+To:<to@example.com>
+Subject: Antw: FW: html
+
+
+OHAI...
+"""), {
+            'text_top': 'hello world',
+            'type': 'forward',
+            'from': '"Some One" <some.one@example.com>',
+            'date': 'Sat, 22 Mar 2008 12:16:06',
+            'subject': 'Antw: FW: html',
+            'to': '<to@example.com>',
+            'text': 'OHAI...',
+        })
+
+
+        # Just a quote
+        self.assertEqual(unwrap("""hello world
+
+Hey: This is very important
+
+> Lorem ipsum
+> dolor sit amet
+> adipiscing elit.
+
+--
+kthxbye
+"""), {
+            'type': 'quote',
+            'text_top': 'hello world\n\nHey: This is very important',
+            'text': 'Lorem ipsum\ndolor sit amet\nadipiscing elit.',
+            'text_bottom': '--\nkthxbye',
+        })
+
+
+        # No message
+        self.assertEqual(unwrap("""hello world
+
+Hey: This is very important
+
+> No quoted message (just one line).
+"""), None)
+
 
 if __name__ == '__main__':
     unittest.main()
